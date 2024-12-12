4b8111c38c858bfd7cff0fa1dd307217e3b0d4b6
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2024-12-12 23:21:23 +0630

    fetch data from projects table

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <RouterLink :to="{ name: '/projects/[id]' , params:{id:1} }">Go to projects</RouterLink>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to projects</RouterLink>
   </div>
 </template>
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
-
 
 </script>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -2,8 +2,24 @@
   <div>
     <h1>Projects Page</h1>
     <RouterLink to="/">Go To Home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
   </div>
 </template>
 <script setup lang="ts">
+
+import { supabase } from '@/libs/superbaseClient.ts';
+import { ref } from 'vue';
+import type { Tables } from '../../../database/types';
+
+const projects = ref <Tables<'projects'>[] | null>(null);
+
+(async () => {
+  const { data, error } = await supabase.from('projects').select();
+  if (error) console.log(error);
+  projects.value = data;
+  console.log(projects.value)
+})();
 
 </script>
