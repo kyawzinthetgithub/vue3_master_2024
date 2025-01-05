aae7e75059f7e3f89533fba4a9527a72b0ae0eaf
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-01-05 08:19:44 +0630

    fetch single project

--- src/utils/supaQueriests.ts
+++ src/utils/supaQueriests.ts
@@ -1,5 +1,5 @@
-import type { QueryData } from '@supabase/supabase-js';
-import { supabase } from '../libs/superbaseClient';
+import type { QueryData } from '@supabase/supabase-js'
+import { supabase } from '../libs/superbaseClient'
 
 export const tasksWithProjectsQuery = supabase.from('tasks').select(`
   *,
@@ -8,10 +8,27 @@ export const tasksWithProjectsQuery = supabase.from('tasks').select(`
     name,
     slug
   )
-`);
+`)
 
-export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>;
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `
+  *,
+  tasks(
+    id,
+    name,
+    status,
+    due_date
+  )
+  `,
+    )
+    .eq('slug', slug)
+    .single();
 
-export const projectsQuery = supabase.from('projects').select();
+export const projectsQuery = supabase.from('projects').select()
 
-export type Projects = QueryData<typeof projectsQuery>;
+export type TasksWithProjects = QueryData<typeof tasksWithProjectsQuery>
+export type Projects = QueryData<typeof projectsQuery>
+export type Project = QueryData<ReturnType<typeof projectQuery>>
