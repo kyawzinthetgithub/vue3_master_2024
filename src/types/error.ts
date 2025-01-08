3c5b8d6a139d2ec177a23949f227ff2b77dbef7b
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-01-08 21:24:12 +0630

    error page fro supabase error

--- src/types/error.ts
+++ src/types/error.ts
@@ -1,3 +1,10 @@
-export interface CustomError extends Error{
-  customCode?:number | undefined
+import type { PostgrestError } from '@supabase/supabase-js'
+
+export interface CustomError extends Error {
+  customCode?: number,
+  statusCode?: number
+}
+
+export interface ExtendedPostgrestError extends PostgrestError {
+  statusCode?: number
 }
