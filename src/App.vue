f965cb3dca53be4503d4f480318725332a41590e
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-01-10 23:09:54 +0630

    error handling development & production

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
-import AppErrorPage from './components/AppError/AppErrorPage.vue';
+const errorStore = useErrorStore()
 
-
-const {activeError} = storeToRefs(useErrorStore());
-
+onErrorCaptured((error) => {
+  errorStore.setError({ error })
+})
 </script>
 
 <template>
   <AuthLayout>
+    <AppErrorPage v-if="errorStore.activeError" />
 
-    <AppErrorPage v-if="activeError"/>
-
-    <RouterView v-else v-slot="{Component,route}">
+    <RouterView v-else v-slot="{ Component, route }">
       <Suspense v-if="Component" :timeout="0">
         <Component :is="Component" :key="route.name"></Component>
+
         <template #fallback>
-          <span>loading...</span>
+          <span>Loading ..</span>
         </template>
       </Suspense>
     </RouterView>
--- src/components/AppError/AppErrorPage.vue
+++ src/components/AppError/AppErrorPage.vue
@@ -1,79 +1,71 @@
 <script setup lang="ts">
+const router = useRouter()
 
-const router = useRouter();
+const errorStore = useErrorStore()
 
-const errorStore = useErrorStore();
-const error = ref(errorStore.activeError);
-const message = ref('');
-const customCode = ref(0);
-const code = ref('');
-const detail = ref('');
-const hint = ref('');
-const statusCode = ref(0);
+const error = ref(errorStore.activeError)
+
+const message = ref('')
+const customCode = ref(0)
+const details = ref('')
+const code = ref('')
+const hint = ref('')
+const statusCode = ref(0)
 
 if (error.value && !('code' in error.value)) {
-  message.value = error.value.message;
+  message.value = error.value.message
   customCode.value = error.value.customCode ?? 0
 }
 
-if(error.value && 'code' in error.value){
-  code.value = error.value.code;
-  message.value = error.value.message;
-  statusCode.value = error.value.statusCode ?? 0;
-  hint.value = error.value.hint;
-  detail.value = error.value.hint;
+if (error.value && 'code' in error.value) {
+  message.value = error.value.message
+  details.value = error.value.details
+  hint.value = error.value.hint
+  code.value = error.value.code
+  statusCode.value = error.value.statusCode ?? 0
 }
 
-router.afterEach(() => {
-  errorStore.activeError = null;
-});
+const ErrorTemplate = import.meta.env.DEV
+  ? defineAsyncComponent(() => import('./AppErrorDevSection.vue'))
+  : defineAsyncComponent(() => import('./AppErrorProdSection.vue'))
 
+router.afterEach(() => {
+  errorStore.clearError()
+})
 </script>
 
 <template>
   <section class="error">
-    <div>
-      <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
-      <h1 class="error__code">{{ customCode || code }}</h1>
-      <p class="error__code" v-if="statusCode">Status Code : {{ statusCode }}</p>
-      <p class="error__msg">{{ message }}</p>
-      <p class="error__msg" v-if="hint">{{ hint }}</p>
-      <p class="error__msg" v-if="detail">{{ detail }}</p>
-      <div class="error-footer">
-        <p class="error-footer__text">You'll find lots to explore on the home page.</p>
-        <RouterLink to="/">
-          <Button class="max-w-36"> Back to homepage </Button>
-        </RouterLink>
-      </div>
-    </div>
+    <ErrorTemplate :message :customCode :code :statusCode :hint :details :isCustomError="errorStore.isCustomError" />
   </section>
 </template>
+
 <style scoped>
 .error {
   @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
 }
 
-.error__icon {
+:deep(.error__icon) {
   @apply text-7xl text-destructive;
 }
 
-.error__code {
+:deep(.error__code) {
   @apply font-extrabold text-7xl text-secondary;
 }
 
-.error__msg {
+:deep(.error__msg) {
   @apply text-3xl font-extrabold text-primary;
 }
 
-.error-footer {
+:deep(.error-footer) {
   @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
 }
 
-.error-footer__text {
+:deep(.error-footer__text) {
   @apply text-lg text-muted-foreground;
 }
 
-p {
+:deep(p) {
   @apply my-2;
 }
 </style>
