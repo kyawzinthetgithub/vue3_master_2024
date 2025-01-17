f7feef17989c3dd079fa5056ff6623d482ce4d1e
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-01-17 22:10:01 +0630

    update cache data if change form database

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -1,23 +1,34 @@
-import type {  Projects } from "@/utils/supaQueriests";
-import { projectsQuery } from "@/utils/supaQueriests";
-import { useMemoize } from "@vueuse/core";
+import type { Projects } from '@/utils/supaQueriests'
+import { projectsQuery } from '@/utils/supaQueriests'
+import { useMemoize } from '@vueuse/core'
 
-export const useProjectsStore = defineStore('projects-store',()=>{
-  const projects = ref<Projects | null>(null);
-  const loadProjects = useMemoize(async (key: string)=>  projectsQuery)
+export const useProjectsStore = defineStore('projects-store', () => {
+  const projects = ref<Projects>([])
+  const loadProjects = useMemoize(async (key: string) => projectsQuery)
 
+  const validateCache = () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data, error }) => {
+        if (JSON.stringify(projects.value) === JSON.stringify(data)) {
+          return
+        } else {
+          loadProjects.delete('projects')
+          if (!error && data) projects.value = data
+        }
+      })
+    }
+  }
 
-const getProjects = async () => {
+  const getProjects = async () => {
+    const { data, error, status } = await loadProjects('projects')
+    if (error) useErrorStore().setError({ error, customCode: status })
+    if(data) projects.value = data
+    validateCache()
+  }
 
-  const { data, error, status } = await loadProjects('projects');
-  if (error) useErrorStore().setError({error,customCode:status});
-  projects.value = data;
-};
-
-return {
-  projects,
-  getProjects,
-  loadProjects
-}
-
-});
+  return {
+    projects,
+    getProjects,
+    loadProjects,
+  }
+})
