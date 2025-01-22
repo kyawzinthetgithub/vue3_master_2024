a7716837dcaa88079483a6991c9181c072f8e7fc
Kyaw Zin Thet <kyawzinthetpersonal@gmail.com>
2025-01-22 23:48:11 +0630

    project status change

--- src/utils/tableColumns/projectsColumns.ts
+++ src/utils/tableColumns/projectsColumns.ts
@@ -1,35 +1,45 @@
-import type { ColumnDef } from "@tanstack/vue-table";
-import type { Projects } from "../supaQueriests";
-import { RouterLink } from "vue-router";
+import type { ColumnDef } from '@tanstack/vue-table'
+import type { Projects } from '../supaQueriests'
+import { RouterLink } from 'vue-router'
 import type { Ref } from 'vue'
 import type { GroupedCollabs } from '@/types/GroupedCollabs'
 import Avatar from '@/components/ui/avatar/Avatar.vue'
 import AvatarImage from '@/components/ui/avatar/AvatarImage.vue'
 import AvatarFallback from '@/components/ui/avatar/AvatarFallback.vue'
+import AppInPlaceEditStatus from '@/components/AppInPlaceEditText/AppInPlaceEditStatus.vue'
 
-export const columns = (
-  collabs: Ref<GroupedCollabs>
-): ColumnDef<Projects[0]>[] => [
+export const columns = (collabs: Ref<GroupedCollabs>): ColumnDef<Projects[0]>[] => [
   {
     accessorKey: 'name',
     header: () => h('div', { class: 'text-left' }, 'Name'),
     cell: ({ row }) => {
-      return h(RouterLink, { to:`/projects/${row.original.slug}`, class: 'text-left hover:bg-muted hover:block w-full' }, () => row.getValue('name'))
-    }
+      return h(
+        RouterLink,
+        {
+          to: `/projects/${row.original.slug}`,
+          class: 'text-left hover:bg-muted hover:block w-full',
+        },
+        () => row.getValue('name'),
+      )
+    },
   },
   {
     accessorKey: 'slug',
     header: () => h('div', { class: 'text-left' }, 'Slug'),
     cell: ({ row }) => {
       return h('div', { class: 'text-left' }, row.getValue('slug'))
-    }
+    },
   },
   {
     accessorKey: 'status',
     header: () => h('div', { class: 'text-left' }, 'Status'),
     cell: ({ row }) => {
-      return h('div', { class: 'text-left' }, row.getValue('status'))
-    }
+      return h(
+        'div',
+        { class: 'text-left' },
+        h(AppInPlaceEditStatus, { modelValue: row.original.status }),
+      )
+    },
   },
   {
     accessorKey: 'collaborators',
@@ -41,19 +51,15 @@ export const columns = (
         collabs.value[row.original.id]
           ? collabs.value[row.original.id].map((collab) => {
               return h(RouterLink, { to: `/users/${collab.username}` }, () => {
-                return h(
-                  Avatar,
-                  { class: 'hover:scale-110 transition-transform' },
-                  () => h(AvatarImage, { src: collab.avatar_url || '' })
+                return h(Avatar, { class: 'hover:scale-110 transition-transform' }, () =>
+                  h(AvatarImage, { src: collab.avatar_url || '' }),
                 )
               })
             })
           : row.original.collaborators.map(() => {
-              return h(Avatar, { class: 'animate-pulse' }, () =>
-                h(AvatarFallback)
-              )
-            })
+              return h(Avatar, { class: 'animate-pulse' }, () => h(AvatarFallback))
+            }),
       )
-    }
-  }
-];
+    },
+  },
+]
